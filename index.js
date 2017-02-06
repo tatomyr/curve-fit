35382900522457cd3d43080d1beaac84c280043e
tatomyr <andrew.tatomyr@gmail.com>
2017-02-07 00:30:53 +0200

    rewrited with charts; minor fixes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fit = (xy, f, cPrev, sPrev, iterations = 1, d) => Array(iterations).fill().reduce((prev, item) => fit.one(xy, f, prev.c, prev.s, d), { c: cPrev, s: sPrev || Infinity });
+const fit = (xy, f, cPrev, sPrev, iterations = 1, d) => Array(iterations).fill().reduce(prev => fit.one(xy, f, prev.c, prev.s, d), { c: cPrev, s: sPrev || Infinity });
 
 fit.s = (xy, f, c) => xy.reduce((sum, point) => sum + (point[1] - f(point[0], c))**2, 0);
 
@@ -10,14 +10,26 @@ fit.one = (xy, f, c, s = Infinity, d = 1) => {
 }
 
 /////////////////////////////////////////
-
-const xy = [
+/*
+let xy = [
   [1,7],
   [2,3],
   [5,4],
 ];
 
-let f = (x, c) => c[0] + c[1] * x;
 let c = [ 10, -1 ];
+let f = (x, c) => c[0] + c[1] * x;
 
 console.log( fit(xy, f, c, Infinity, 10) );
+
+
+let data = `
+  1 12
+  7 14
+  12 5
+  41 7
+`;
+xy = data.trim().split('\n').filter(item => item).map(item => item.trim().split(/ +/));
+console.log(xy);
+console.log( fit(xy, f, c, Infinity, 10) );
+*/
