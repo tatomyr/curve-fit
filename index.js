4d6348914ade3881d0c8d53d76cf6287180d349e
tatomyr <andrew.tatomyr@gmail.com>
2017-02-06 02:46:58 +0200

    small improvement

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const fit = (xy, f, cPrev, sPrev, iterations = 1, d) => Array(iterations).fill()
 
 fit.s = (xy, f, c) => xy.reduce((sum, point) => sum + (point[1] - f(point[0], c))**2, 0);
 
-fit.one = (xy, f, c, s = Infinity, d) => {
-  const delta = d || c.map(item => 1);
+fit.one = (xy, f, c, s = Infinity, d = 1) => {
+  const delta = typeof d === 'array' && d || typeof d === 'number' && c * d || c.map(item => 1);
   const c_ = c.map((item, i) => item + (Math.random() - Math.random()) * delta[i]);
   const s_ = fit.s(xy, f, c_);
-  // return s_ < sPrev ? c_ : c;
   return s_ < s ? { c: c_, s: s_ } : { c, s };
 }
 
@@ -21,4 +20,4 @@ const xy = [
 let f = (x, c) => c[0] + c[1] * x;
 let c = [ 10, -1 ];
 
-console.log( fit(xy, f, c, Infinity, 100, c*0.1) );
+console.log( fit(xy, f, c, Infinity, 10) );
